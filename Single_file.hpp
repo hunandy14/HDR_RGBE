4e15946f6cfa26b82623c73a27979db158bfa4d6
CharlotteHonG <hunandy14@gmail.com>
2017-05-23 18:04:04 +0800

    新增.hpp

--- Single_file.hpp
+++ Single_file.hpp
@@ -4,5 +4,8 @@ Date : 2017/05/23
 By   : CharlotteHonG
 Final: 2017/05/23
 *****************************************************************/
+#pragma once
+
+#include "Rgbe.hpp"
 #include "Rgbe.cpp"
 #include "Rgbe2Raw.cpp"
